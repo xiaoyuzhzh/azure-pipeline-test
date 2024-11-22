7eaa79bf570bab24a6c000884497ecc5e8986619
xiaoyuzhzh <xiaoyuzhzh@outlook.com>
2024-11-22 13:58:42 +0800

    修复编译错误

--- src/main/java/com/xiaoyuzhzh/apiserver/GreetingController.java
+++ src/main/java/com/xiaoyuzhzh/apiserver/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
